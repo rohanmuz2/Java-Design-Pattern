206cf65b987211617f53e83739f5417d6554232c
Nisaar <rohan.muz2@gmail.com>
2022-12-19 21:42:42 +0530

    First Version

--- src/SimpleFactory/products/VegBurger.java
+++ src/SimpleFactory/products/VegBurger.java
@@ -2,6 +2,11 @@ package SimpleFactory.products;
 
 public class VegBurger implements Burger{
 
+
+    public void test(){
+       
+    }
+
     @Override
     public void orderBurger() {
         System.out.println("Veg");
